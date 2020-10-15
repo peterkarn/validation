d2b593f622a2ba6a57106e0d5b38c7571113d1ff
peterkarnaukhsite <62539689+peterkarnaukhsite@users.noreply.github.com>
2020-10-15 20:59:39 +0300

    Update main.js
    // phone input validation  added on string #109

--- main.js
+++ main.js
@@ -103,12 +103,33 @@
     $('.code-mask').mask('000000', {
       placeholder: code_placeholder
     });
-
-    //added mask function for #user_name input
-     $('#user_name').mask('+0000000000000', {
-      placeholder: '+0000000000000'
-    });
+    
   }
+  
+// phone input validation  
+var inputEl = document.getElementById('phone');
+var goodKey = '0123456789+ ';
+
+var checkInputTel = function(e) {
+  var key = (typeof e.which == "number") ? e.which : e.keyCode;
+  var start = this.selectionStart,
+    end = this.selectionEnd;
+
+  var filtered = this.value.split('').filter(filterInput);
+  this.value = filtered.join("");
+
+  /* Prevents moving the pointer for a bad character */
+  var move = (filterInput(String.fromCharCode(key)) || (key == 0 || key == 8)) ? 0 : 1;
+  this.setSelectionRange(start - move, end - move);
+}
+
+var filterInput = function(val) {
+  return (goodKey.indexOf(val) > -1);
+}
+
+inputEl.addEventListener('input', checkInputTel);
+
+$("#phone").attr('maxlength', '12');
 
   // Navbar collapse in header
   function navbarCollapse() {
